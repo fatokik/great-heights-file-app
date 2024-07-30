98d0aae57f87e0713060b205cb6a3e4c92d6ab04
Kenny Fatoki <fatokik@gmail.com>
2024-07-29 22:58:37 -0400

    few more changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,18 @@
-import Image from "next/image";
+"use client";
 
 export default function Home() {
-  return <main>Home</main>;
+  return (
+    <div className="min-h-screen bg-gray-100 p-4">
+      <header>
+        <div className="container mx-auto p-4">
+          <h1 className="text-4xl font-bold">Welcome User</h1>
+        </div>
+      </header>
+      <main className="container mx-auto mt-6">
+        <h2 className="text-l font-semibold">
+          Select an item from the menu to get started.
+        </h2>
+      </main>
+    </div>
+  );
 }
--- components/NavBar/Navbar.tsx
+++ components/NavBar/Navbar.tsx
@@ -1,23 +1,85 @@
 "use client";
 import { FC } from "react";
 
-import { RocketIcon, AccessibilityIcon, RowsIcon } from "@radix-ui/react-icons";
+import {
+  RocketIcon,
+  AccessibilityIcon,
+  RowsIcon,
+  EyeOpenIcon,
+  PinTopIcon,
+  PersonIcon,
+} from "@radix-ui/react-icons";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import { Button } from "../ui/button";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuShortcut,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
 
 export const Navbar: FC = () => {
   return (
-    <div className="w-full border-b border-gray-300 h-12 flex flex-row justify-between px-6">
+    <div className="w-full border-b border-gray-300 h-12 flex flex-row justify-between px-4">
       <div className="flex flex-row gap-4 justify-center items-center">
-        <RocketIcon className="h-8 w-8 " />
+        <RocketIcon className="h-8 w-8 text-blue-300" />
         <p className="font-bold">Great Heights</p>
-        <RowsIcon className="h-4 w-4" />
+
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" size="icon">
+              <RowsIcon className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+
+          <DropdownMenuContent className="w-40">
+            <DropdownMenuGroup>
+              <DropdownMenuItem>
+                File Viewer
+                <DropdownMenuShortcut>
+                  <EyeOpenIcon />
+                </DropdownMenuShortcut>
+              </DropdownMenuItem>
+
+              <DropdownMenuItem>
+                File Upload
+                <DropdownMenuShortcut>
+                  <PinTopIcon />
+                </DropdownMenuShortcut>
+              </DropdownMenuItem>
+
+              <DropdownMenuItem>
+                Admin
+                <DropdownMenuShortcut>
+                  <PersonIcon />
+                </DropdownMenuShortcut>
+              </DropdownMenuItem>
+            </DropdownMenuGroup>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
 
       <div className="flex flex-row gap-5 items-center">
-        <AccessibilityIcon className="h-6 w-6" />
+        <Popover>
+          <PopoverTrigger asChild>
+            <Button variant="ghost" size="icon">
+              <AccessibilityIcon className="h-6 w-6" />
+            </Button>
+          </PopoverTrigger>
+
+          <PopoverContent>
+            <p>
+              For any help regarding this application, please reach out to
+              [\`test@great-heights.com\`]
+            </p>
+          </PopoverContent>
+        </Popover>
         <Avatar className="h-89 w-9">
           <AvatarImage src="https://github.com/shadcn.png" />
-          <AvatarFallback>CN</AvatarFallback>
+          <AvatarFallback>KF</AvatarFallback>
         </Avatar>
       </div>
     </div>
