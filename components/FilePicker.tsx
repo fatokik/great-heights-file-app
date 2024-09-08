eee6d77a6893f74117c9dd0b5f1547889893d214
Kenny Fatoki <fatokik@gmail.com>
2024-09-08 15:47:41 -0500

    finished up file picker

--- components/FilePicker.tsx
+++ components/FilePicker.tsx
@@ -13,7 +13,8 @@ interface FilePickerProps {
 export const FilePicker: React.FC<FilePickerProps> = (
   props: FilePickerProps
 ) => {
-  const [hover, setHover] = useState(false);
+  const [fileDivhover, setFileDivHover] = useState(false);
+  const [deleteFileHover, setDeleteFileHover] = useState(false);
   const [files, setFiles] = useState<FileList | null>(null);
   const [fileText, setFileText] = useState(
     "Select or drag and drop multiple files"
@@ -21,27 +22,35 @@ export const FilePicker: React.FC<FilePickerProps> = (
 
   const fileInputRef = useRef<HTMLInputElement | null>(null);
 
-  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const currFiles = event.target.files ? event.target.files : null;
+  const handleFileChange = (files: FileList | null) => {
+    if (files) {
+      if (files.length > 0) {
+        props.onChange(files);
+        setFiles(files);
 
-    if (currFiles && currFiles.length > 0) {
-      props.onChange(currFiles);
-
-      setFiles(currFiles);
-      if (files) {
         files.length === 1
           ? setFileText(`${files.length} file selected`)
           : setFileText(`${files.length} files selected`);
       } else {
-        setFileText("Select or drag and drop multiple files");
+        clearFilePicker();
+        props.onChange(null);
       }
     } else {
       clearFilePicker();
+      props.onChange(null);
     }
+  };
+
+  const onFileDivClick = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const currFiles = event.target.files;
+    handleFileChange(currFiles);
   };
 
   const clearFilePicker = () => {
     setFiles(null);
+    setFileText("Select or drag and drop multiple files");
+
+    if (fileInputRef.current) fileInputRef.current.value = "";
   };
 
   const handleFileClick = () => {
@@ -50,30 +59,28 @@ export const FilePicker: React.FC<FilePickerProps> = (
     }
   };
 
+  const handleDeleteButtonClick = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    event.stopPropagation();
+    clearFilePicker();
+  };
+
   const handleDragOver = (event: React.DragEvent<HTMLDivElement>) => {
     event.preventDefault();
   };
 
   const handleDragEnter = (event: React.DragEvent<HTMLDivElement>) => {
     event.preventDefault();
-    setHover(true);
-  };
-
-  const handleDragLeave = () => {
-    setHover(false);
+    setFileDivHover(true);
   };
 
   const handleDrop = (event: React.DragEvent<HTMLDivElement>) => {
     event.preventDefault();
-    setHover(false);
+    setFileDivHover(false);
 
     const droppedFiles = event.dataTransfer.files;
-    if (droppedFiles) {
-      props.onChange(droppedFiles);
-      droppedFiles.length === 1
-        ? setFileText(`${droppedFiles.length} file selected`)
-        : setFileText(`${droppedFiles.length} files selected`);
-    }
+    handleFileChange(droppedFiles);
   };
 
   return (
@@ -81,13 +88,13 @@ export const FilePicker: React.FC<FilePickerProps> = (
       className={cn(
         "flex items-center justify-center border-2 border-dotted border-gray-500 rounded-lg",
         "min-h-[10vh] w-[250px]",
-        hover && "bg-blue-50"
+        fileDivhover && "bg-blue-50"
       )}
-      onMouseEnter={() => setHover(true)}
-      onMouseLeave={() => setHover(false)}
+      onMouseEnter={() => setFileDivHover(true)}
+      onMouseLeave={() => setFileDivHover(false)}
       onDragOver={handleDragOver}
       onDragEnter={handleDragEnter}
-      onDragLeave={handleDragLeave}
+      onDragLeave={() => setFileDivHover(false)}
       onDrop={handleDrop}
       onClick={handleFileClick}
     >
@@ -97,18 +104,22 @@ export const FilePicker: React.FC<FilePickerProps> = (
             ref={fileInputRef}
             multiple
             type="file"
-            onChange={handleFileChange}
+            onChange={onFileDivClick}
             className="hidden"
           />
           <PinTopIcon />
         </Button>
         <p className="text-xs">{fileText}</p>
         {files && (
-          <>
-            <Button variant={"ghost"} size={"sm"} onClick={clearFilePicker}>
-              <TrashIcon />
-            </Button>
-          </>
+          <Button
+            variant={"ghost"}
+            size={"sm"}
+            onClick={handleDeleteButtonClick}
+            onMouseEnter={() => setDeleteFileHover(true)}
+            onMouseLeave={() => setDeleteFileHover(false)}
+          >
+            <TrashIcon className={cn(deleteFileHover && "text-red-500")} />
+          </Button>
         )}
       </div>
     </div>
